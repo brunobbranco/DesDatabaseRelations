0a8ddd9011029206e5814c9eeb8d8dbd1010de27
Bruno Branco <brunobbranco@gmail.com>
2020-05-28 03:31:03 -0300

    Finish desafio09

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -1,8 +1,9 @@
-import { getRepository, Repository, In } from 'typeorm';
+import { getRepository, Repository, In, UpdateQueryBuilder } from 'typeorm';
 
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
 import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
+import AppError from '@shared/errors/AppError';
 import Product from '../entities/Product';
 
 interface IFindProducts {
@@ -21,21 +22,71 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const product = await this.ormRepository.findOne({ where: { name } });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const productsId = products.map(product => product.id);
+    const findProducts = await this.ormRepository.find({
+      id: In(productsId),
+    });
+
+    if (productsId.length !== findProducts.length) {
+      throw new AppError('An id was not found');
+    }
+
+    return findProducts;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const productsData = await this.ormRepository.findByIds(products);
+
+    const listProducts = productsData.map(productData => {
+      const foundProduct = products.find(
+        product => product.id === productData.id,
+      );
+      if (!foundProduct) {
+        throw new AppError('Product not found');
+      }
+      if (productData.quantity < foundProduct.quantity) {
+        throw new AppError('Insuficient quantity to a product');
+      }
+
+      const retProduct = productData;
+      retProduct.quantity -= foundProduct.quantity;
+
+      return retProduct;
+    });
+
+    await this.ormRepository.save(listProducts);
+
+    return listProducts;
+
+    // const updatedProducts: Product[] = [];
+    // products.map(loopProducts => async (): Promise<Product[]> => {
+    //   const retUpdate = await this.ormRepository
+    //     .createQueryBuilder('updateQuantity')
+    //     .update()
+    //     .set({ quantity: loopProducts.quantity })
+    //     .where('id = :id', { id: loopProducts.id });
+    //   updatedProducts.push(retUpdate);
+    // });
   }
 }
 
---
+++ src/shared/infra/typeorm/migrations/1590634169990-CreateOrdersProducts.ts
@@ -0,0 +1,73 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateOrdersProducts1590634169990
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders_products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'product_id',
+            type: 'uuid',
+            isNullable: false,
+          },
+          {
+            name: 'order_id',
+            type: 'uuid',
+            isNullable: false,
+          },
+          {
+            name: 'price',
+            type: 'numeric',
+            precision: 15,
+            scale: 2,
+          },
+          {
+            name: 'quantity',
+            type: 'integer',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FKOrderProducts_Orders',
+            columnNames: ['order_id'],
+            referencedTableName: 'orders',
+            referencedColumnNames: ['id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+          {
+            name: 'FKOrderProducts_Products',
+            columnNames: ['product_id'],
+            referencedTableName: 'products',
+            referencedColumnNames: ['id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orders_products');
+  }
+}
